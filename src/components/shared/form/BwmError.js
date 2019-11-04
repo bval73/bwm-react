e8e0cab5df82f2b16e9319ff328f9655673b5b70
bval73 <52051327+bval73@users.noreply.github.com>
2019-11-04 14:01:16 -0500

    adds booking component and booking creation functionality. Adds toast messages and confirmation modal

--- src/components/shared/form/BwmError.js
+++ src/components/shared/form/BwmError.js
@@ -10,4 +10,3 @@ export function BwmErrors(props) {
       </div>
   )
 }
-
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,22 @@
 import titleize from 'titleize';
+import * as moment from 'moment';
 
 
 export const rentalType = isShared => isShared ? 'sharen' : 'entire'
 
 export const toUpperCase = value => value ? titleize(value) : ''
+
+export const getRangeOfDates = (startAt, endAt, dateFormat = 'Y/MM/DD') => {
+  const tempDates = [];
+  const mEndDt = moment(endAt);
+  let mStartAt = moment(startAt);
+
+  while(mStartAt < mEndDt) {
+    tempDates.push(mStartAt.format(dateFormat));
+    mStartAt = mStartAt.add(1, 'day');
+  }
+
+  tempDates.push(mEndDt.format(dateFormat));
+
+  return tempDates;
+}
