b6abf0d7a4bb47d7c855b754715e5c3b4185f9bf
bval73 <52051327+bval73@users.noreply.github.com>
2020-01-28 15:00:59 -0500

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
  DB_URI: 'mongodb+srv://test:testtest@cluster0-cx1xu.mongodb.net/test?retryWrites=true&w=majority',
  SECRET: 'nafipuhbrp',
- AWS_ACCESS_KEY_ID: 'AKIAJ7VUZYOMVEUCSCQQ',
- AWS_SECRET_ACCESS_KEY: 'pQmF8z985U2P84NwbzlUee3pVH/uncSdy3iWT0NP'
+ AWS_ACCESS_KEY_ID: 'add your id',
+ AWS_SECRET_ACCESS_KEY: 'add your key'
 }
