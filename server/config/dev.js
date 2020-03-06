1c94f392732e64c1fc7b8cfea61d0656e48480a6
bval73 <52051327+bval73@users.noreply.github.com>
2020-03-06 12:06:59 -0500

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
- DB_URI: 'mongodb+srv://test:testtest@cluster0-cx1xu.mongodb.net/test?retryWrites=true&w=majority',
- SECRET: 'nafipuhbrp',
- AWS_ACCESS_KEY_ID: 'AKIAJ7VUZYOMVEUCSCQQ',
- AWS_SECRET_ACCESS_KEY: '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',
- STRIPE_SK: 'sk_test_jXsRjOIJNXS6gPmu8hiiypsG00iW7ldShh',
- STRIPE_PK: 'pk_test_OBoqi5fgE5xFWa9ucpMW5XiN00IFO0lbNY'
-}
-
